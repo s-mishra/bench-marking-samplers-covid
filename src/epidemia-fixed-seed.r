66f14969e932a5e85b6e51375a6b15ff1a6724a7
Seth Flaxman <s.flaxman@imperial.ac.uk>
2020-11-02 17:09:04 +0000

    4000 samples takes 3.5 hours

--- src/epidemia-fixed-seed.r
+++ src/epidemia-fixed-seed.r
@@ -6,4 +6,5 @@ options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 m = stan_model(here("stan-models/epidemia_fixed_seed.stan"))
 
-fit = sampling(m, data=data, iter=1000, seed=6, control = list(max_treedepth=15, adapt_delta=0.95))
+fit = sampling(m, data=data, iter=4000, control = list(max_treedepth=20, adapt_delta=0.99),thin=4,seed=1)
+
