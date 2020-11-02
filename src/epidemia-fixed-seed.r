24a2badc130d031947a1549348017bcab579c2cb
Seth Flaxman <s.flaxman@imperial.ac.uk>
2020-11-02 17:34:15 +0000

    shorter time series to go faster

--- src/epidemia-fixed-seed.r
+++ src/epidemia-fixed-seed.r
@@ -6,5 +6,5 @@ options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 m = stan_model(here("stan-models/epidemia_fixed_seed.stan"))
 
-fit = sampling(m, data=data, iter=400, control = list(max_treedepth=20, adapt_delta=0.99),thin=4,seed=1)
+fit = sampling(m, data=data, iter=400, control = list(max_treedepth=15, adapt_delta=0.9),thin=4,seed=1)
 
