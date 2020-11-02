dc4ee8d8f820238cdf865eeff2680779d3ba40ad
Seth Flaxman <s.flaxman@imperial.ac.uk>
2020-11-02 15:58:28 +0000

    epidemia's code

---
+++ src/epidemia-base.r
@@ -0,0 +1,9 @@
+library(here)
+data = readRDS(here("src/epidemia_fixed_seed.rds"))
+library(rstan)
+
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+m = stan_model(here("stan-models/epidemia_base.stan"))
+
+fit = sampling(m, data=data, iter=800, seed=8, control = list(max_treedepth=15, adapt_delta=0.95))
