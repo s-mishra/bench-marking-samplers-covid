e20d6ccf91a0a862837d82ba213d52dfa499411d
Seth Flaxman <s.flaxman@imperial.ac.uk>
2020-11-02 17:28:25 +0000

    smaller dataset to speedup sampling

--- src/epidemia-base.r
+++ src/epidemia-base.r
@@ -6,4 +6,4 @@ options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 m = stan_model(here("stan-models/epidemia_base.stan"))
 
-fit = sampling(m, data=data, iter=1000, seed=8, control = list(max_treedepth=15, adapt_delta=0.95))
+fit = sampling(m, data=data, iter=400, seed=8, control = list(max_treedepth=15, adapt_delta=0.95))
